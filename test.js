972d9ab1695fa31431b6872c62b0fa023badcdda
Ayanokojiii <69939035+Ayanokojiii@users.noreply.github.com>
2022-08-07 11:25:54 +0200

    Update test.js

--- test.js
+++ test.js
@@ -12,24 +12,6 @@ function set_up_pagination(array, templateMessage){
       </li>`;
     $('#search-module-pagination li').last().before(newButPage)
 
-    if($('[name="type"]').eq(0).val() == 'message-a2rm'){
-
-      gPageNumber++
-      gUrl = 'https://avenoel.org/forum/' + gPageNumber
-      entry(gUrl)
-
-      numberMatchMessage++
-      var currentMessNb = numberMatchMessage
-      function checkHere(){
-        containerObserver = setInterval(function(){
-          if(currentMessNb == numberMatchMessage){
-            entry(gUrl)
-          }
-        },500)
-      }
-      checkHere()
-    }
-
     //set up new page
 
     let newPage = `<div page-number="${newButPageIndex}" class="page-group page-group-${newButPageIndex} pagination-overwrite"></div>`;
@@ -38,7 +20,6 @@ function set_up_pagination(array, templateMessage){
     $('#search-module-pagination li a').click((e)=>{
       e.preventDefault()
     })
-
   }
   $("#data-container .page-group").last().append(templateMessage)
   hideSpinner()
@@ -49,6 +30,24 @@ function set_up_pagination(array, templateMessage){
       $(`.page-group[page-number=${i}]`).removeClass('display-none');
     })
   })
+
+  if($('[name="type"]').eq(0).val() == 'message-a2rm'){
+
+    gPageNumber++
+    gUrl = 'https://avenoel.org/forum/' + gPageNumber
+    entry(gUrl)
+
+    numberMatchMessage++
+    var currentMessNb = numberMatchMessage
+    function checkHere(){
+      containerObserver = setInterval(function(){
+        if(currentMessNb == numberMatchMessage){
+          entry(gUrl)
+        }
+      },500)
+    }
+    checkHere()
+  }
 
 }
 
